f8504fcdc13c5e0b21c0fffe349f7f56e4815d63
lucasferreira16-tech <lucas.ferreira16@estudante.ifms.edu.br>
2026-04-23 10:53:48 -0400

    cm

--- backend/src/shared/databases/prisma.database.ts
+++ backend/src/shared/databases/prisma.database.ts
@@ -1,11 +1,8 @@
-import { Injectable } from "@nestjs/common";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
 
 
-
-
-@Injectable()
  export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
--- backend/src/todos/todos.service.ts
+++ backend/src/todos/todos.service.ts
@@ -1,37 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { CreateTodoRepository, DeleteTodoRepository, FindAllTodoRepository, FindUniqueTodoRepository, UpdateTodoRepository } from './repository';
-
+import { DeleteTodoRepository, FindAllTodoRepository, FindUniqueTodoRepository, UpdateTodoRepository } from './repository';
+import { CreateTodoUseCase } from './use-cases';
 
 
 @Injectable()
 export class TodosService {
-       private readonly createTodoReporitory: CreateTodoRepository;
-       private readonly  findAllTodoRepository: FindAllTodoRepository;
-       private readonly findUniqueTodoRepository: FindUniqueTodoRepository;
-       private readonly updateTodoRepository: UpdateTodoRepository;
-        private readonly deleteTodoRepository: DeleteTodoRepository;
-  async  create(data: CreateTodoDto) {
-      return await this.createTodoReporitory.create(data);
-    
+  constructor(
+    private readonly createTodoUseCase: CreateTodoUseCase,
+    private readonly findAllTodoRepository: FindAllTodoRepository,
+    private readonly findUniqueTodoRepository: FindUniqueTodoRepository,
+    private readonly updateTodoRepository: UpdateTodoRepository,
+    private readonly deleteTodoRepository: DeleteTodoRepository,
+  ) {}
+
+  async create(data: CreateTodoDto) {
+    return await this.createTodoUseCase.execute(data);
   }
 
   async findAll() {
-      return await this.findAllTodoRepository.findAll();
-    
+    return await this.findAllTodoRepository.findAll();
   }
 
- async findOne(id: string) {
-      return await this.findUniqueTodoRepository.findById(id);
-    
+  async findOne(id: string) {
+    return await this.findUniqueTodoRepository.findById(id);
   }
 
- async update(id: string, data: UpdateTodoDto) {
-      return await this.updateTodoRepository.update(id, data);
+  async update(id: string, data: UpdateTodoDto) {
+    return await this.updateTodoRepository.update(id, data);
   }
 
- async remove(id: string) {
-      return await this.deleteTodoRepository.delete(id);
+  async remove(id: string) {
+    return await this.deleteTodoRepository.delete(id);
   }
 }
