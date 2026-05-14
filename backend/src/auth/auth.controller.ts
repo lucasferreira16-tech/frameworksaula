f5f4b6d371a573d9ee46d567f48ad3377ad07d34
lucasferreira16-tech <lucas.ferreira16@estudante.ifms.edu.br>
2026-05-14 14:04:21 -0400

    gfghvfghg

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,1 +1,25 @@
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { RegisterDto } from "./dto/register.dto";
+import { AuthService } from "./auth.service";
+import { LoginDto } from "./dto/login.dto";
+
 // aqui é o 
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+@Post('Register')
+async register(@Body() body: RegisterDto) {
+    return await this.authService.register(data);
+}
+
+@Post('login')
+async login(@Body() data: LoginDto) {
+    return await this.authService.login(data);
+}
+
+//@UseGuards(JwtAuthGuard)
+//@Get('me')
+//async me(@CurrentUser() user: { id: string; email: string}) {
+//    return user;
+//}
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,1 +1,20 @@
-// aqui é o 
+import { Injectable } from "@nestjs/common";
+import { LoginUseCase, RegisterUseCase } from "./use-cases";
+import { RegisterDto } from "./dto/register.dto";
+import { LoginDto } from "./dto/login.dto";
+
+//aqui
+@Injectable()
+export class AuthService{
+    constructor(private readonly registerUseCase: RegisterUseCase,
+        private readonly loginUseCase: LoginUseCase,
+    ) {}
+
+    async register(data: RegisterDto){
+        return await this.registerUseCase.execute(data);
+    }
+
+    async login(data: LoginDto) {
+        return await this.loginUseCase.execute(data);
+    }
+}
